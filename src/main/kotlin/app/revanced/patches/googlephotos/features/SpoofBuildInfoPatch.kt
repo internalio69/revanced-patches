8a988fef25ca56deedf37b707ae26fb4ad7a4fe5
internalio69 <124493841+internalio69@users.noreply.github.com>
2024-10-10 22:53:36 +0530

    Update SpoofBuildInfoPatch.kt

--- src/main/kotlin/app/revanced/patches/googlephotos/features/SpoofBuildInfoPatch.kt
+++ src/main/kotlin/app/revanced/patches/googlephotos/features/SpoofBuildInfoPatch.kt
@@ -3,12 +3,12 @@ package app.revanced.patches.googlephotos.features
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.all.misc.build.BaseSpoofBuildInfoPatch
 
-@Patch(description = "Spoof build info to Google Pixel XL.")
+@Patch(description = "Spoof build info to Google Pixel 1.")
 internal class SpoofBuildInfoPatch : BaseSpoofBuildInfoPatch() {
     override val brand = "google"
     override val manufacturer = "Google"
-    override val device = "marlin"
-    override val product = "marlin"
-    override val model = "Pixel XL"
-    override val fingerprint = "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
+    override val device = "sailfish"
+    override val product = "sailfish"
+    override val model = "Pixel"
+    override val fingerprint = "google/sailfish/sailfish:10/QP1A.191005.007.A3/5972272:user/release-keys"
 }
